ba6eac033961e0c258376aee043eb05483f5ea1c
manvet <alskdjaksjd@sdkfhj.com>
2018-06-14 10:03:36 +0200

    TimerPageViewModel hinzugefügt

---
+++ TimerApp/TimerApp/ViewModel/TimerPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using TimerApp.Model;
+
+namespace TimerApp.ViewModel
+{
+    
+
+    class TimerPageViewModel : INotifyPropertyChanged
+    {
+        public DisplayTimer CurrentTimer { get; set; }
+
+        public TimerPageViewModel()
+        {
+            CurrentTimer = new DisplayTimer();
+            CurrentTimer.Duration = new TimeSpan(10000);
+            CurrentTimer.Name = "Uebung01";
+            CurrentTimer.Repetitions = 5;
+        }
+
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged == null)
+                return;
+
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
