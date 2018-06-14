169e6a284f7ab90c5eaf0312451c038bce7d57c0
manvet <alskdjaksjd@sdkfhj.com>
2018-06-14 10:03:07 +0200

    DisplayTimer definiert

---
+++ TimerApp/TimerApp/Model/DisplayTimer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimerApp.Model
+{
+    class DisplayTimer
+    {
+        private string name;
+        private TimeSpan duration;
+        private uint repetitions;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public TimeSpan Duration
+        {
+            get { return duration; }
+            set { duration = value; }
+        }
+        public uint Repetitions
+        {
+            get { return repetitions; }
+            set { repetitions = value; }
+        }
+    }
+}
